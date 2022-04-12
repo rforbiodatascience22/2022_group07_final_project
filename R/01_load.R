3b06229bd41f98c7311ce5a9c8c559e9d20051ad
FedeRapelli <s210156@student.dtu.dk>
2022-04-12 16:11:14 +0000

    data wrangling

--- R/01_load.R
+++ R/01_load.R
@@ -1,6 +1,14 @@
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
-
+library("viridis")
+library("patchwork")
+library("ggridges")
+library("fs")
+library("caret")
+library("purrr")
+library("broom")
+library("dplyr")
+library(cowplot)
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
@@ -18,3 +26,12 @@ my_data <- my_data_raw # %>% ...
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data,
           file = "data/01_my_data.tsv")
+
+expression_raw<- expression_raw %>% 
+          pivot_wider(id_cols = Monarch,
+                      id_expand = FALSE,
+                      names_from = Gene,
+                      values_from = "LogRelexpr18S28S",
+                                      "Groupnumber",
+                                      "Sex",
+                                      "Population")
