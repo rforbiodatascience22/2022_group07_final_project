ed4bc14728e6155e7945d4f43c1a67d7ca7c24d8
Dimitrios Loukas <s220514@dtu.dk>
2022-04-12 20:45:56 +0300

    Revert "Revert "Merge pull request #1 from rforbiodatascience22/siddhi_load""
    This reverts commit 034f583db195358c9c3b616cf39fc749884b89d5.

--- R/01_load.R
+++ R/01_load.R
@@ -3,7 +3,7 @@ library("tidyverse")
 
 
 # Define functions --------------------------------------------------------
-source(file = "R/99_project_functions.R")
+#source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
@@ -12,9 +12,18 @@ expression_raw <- read.csv(file = "data/_raw/Talla_East_West_North_America_Monar
 
 
 # Wrangle data ------------------------------------------------------------
-my_data <- my_data_raw # %>% ...
+#Pivoting expression data to obtain one column for every gene
+data_wide_expression <- expression_raw %>% 
+  pivot_wider(id_cols = Monarch,
+              names_from = Gene,
+              values_from = c(LogRelexpr18S28S,Groupnumber,Population,Sex))
 
+#Renaming ID column so that both dataframes match
+data_wide_expression <- rename(data_wide_expression, "ID" = "Monarch")
+
+#Joining expression and morphology data by ID
+final_data <- full_join(data_wide_expression,morphology_raw,by="ID")
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data,
+write_tsv(x = final_data,
           file = "data/01_my_data.tsv")
