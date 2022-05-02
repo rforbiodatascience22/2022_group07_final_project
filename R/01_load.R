8c869409895dee2e3c2ee8cf1f38041616b26e83
siddhijain25 <siddhiyjain@gmail.com>
2022-05-02 09:22:26 +0000

    cleaning the code

--- R/01_load.R
+++ R/01_load.R
@@ -4,8 +4,10 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-morphology_raw <- as_tibble(read.csv(file = "data/_raw/Talla_East_West_North_America_Monarch_Genomics_Flight_And_Wing_Morphology_Data.csv"))
-expression_raw <- as_tibble(read.csv(file = "data/_raw/Talla_East_West_North_America_Monarch_Genomics_Gene_Expression_Data.csv"))
+morphology_raw <- as_tibble(read.csv(file = "data/_raw/Talla_East_West_North
+                                     _America_Monarch_Genomics_Flight_And_Wing_Morphology_Data.csv"))
+expression_raw <- as_tibble(read.csv(file = "data/_raw/Talla_East_West_North
+                                     _America_Monarch_Genomics_Gene_Expression_Data.csv"))
 
 
 # Wrangle data ------------------------------------------------------------
@@ -13,22 +15,33 @@ expression_raw <- as_tibble(read.csv(file = "data/_raw/Talla_East_West_North_Ame
 #Pivoting expression data to obtain one column for every gene
 #The variables already present in the other dataset are not included
 #Groupnumber is not informative and is not included
+
 expression_wide <- expression_raw %>% 
   pivot_wider(id_cols = Monarch,
               names_from = Gene,
-              values_from = c(LogRelexpr18S28S, Groupnumber, Population, Sex))
+              values_from = c(LogRelexpr18S28S, 
+                              Groupnumber, 
+                              Population, 
+                              Sex))
 
 
 #Renaming ID column so that both dataframes match
-expression_wide <- rename(expression_wide, "ID" = "Monarch")
+expression_wide <- rename(expression_wide, 
+                          "ID" = "Monarch")
 
 #Joining expression and morphology data by ID
-final_data <- full_join(expression_wide,morphology_raw,by="ID")
+final_data <- full_join(expression_wide,
+                        morphology_raw,
+                        by = "ID")
 
 # Renaming PC columns
-final_data <- rename(final_data, "PC1_wing_size" = "PC1")
-final_data <- rename(final_data, "PC2_wing_shape" = "PC2")
-names(final_data) <- str_replace(names(final_data), "LogRelexpr18S28S_", "Gene_")
+final_data <- rename(final_data, 
+                     "PC1_wing_size" = "PC1")
+final_data <- rename(final_data, 
+                     "PC2_wing_shape" = "PC2")
+names(final_data) <- str_replace(names(final_data),
+                                 "LogRelexpr18S28S_", 
+                                 "Gene_")
 
 # Write data --------------------------------------------------------------
 write_tsv(x = final_data,
